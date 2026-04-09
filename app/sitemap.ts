9bdf53e91ece1829070d74a71280062ecf3abcaf
Manhiru <Manhiru21@gmail.com>
2026-04-10 00:52:36 +0100

    SEO: remove doorway pages as i didnt look at guidelines properly

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,23 +1,14 @@
 import { MetadataRoute } from 'next'
-import localTrades from './data/localTrades.json'
 
 const BASE_URL = 'https://novark-agency.co.uk';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-    const tradeRoutes: MetadataRoute.Sitemap = localTrades.map((trade) => ({
-        url: `${BASE_URL}/trades/${trade.slug}`,
-        lastModified: new Date(),
-        changeFrequency: 'daily',
-        priority: 0.8,
-    }));
-
     return [
         {
             url: BASE_URL,
             lastModified: new Date(),
-            changeFrequency: 'daily',
+            changeFrequency: 'weekly',
             priority: 1,
-        },
-        ...tradeRoutes,
+        }
     ]
 }
